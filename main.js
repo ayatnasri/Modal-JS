3e47e3a1231aff878dc4a3dafed75d7bea07c44c
ayatnasri <aayat.nasri@gmail.com>
2022-02-24 02:15:19 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const modalBtn = document.querySelector(".modal-btn");
+const modalOver = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click", function(){
+    modalOver.classList.add("open-modal");
+});
+closeBtn.addEventListener("click", function(){
+    modalOver.classList.remove("open-modal");
+});
